59f7bd658794bfe219dfb353b0d5ef3911b0c958
Nazir <n_ahmad20@live.co.uk>
2019-07-15 20:38:09 +0100

    Add ProductsService / Add Product Controller with View and Create

---
+++ Ecommerce.Services/ProductsService.cs
@@ -0,0 +1,67 @@
+﻿using Ecommerce.Database;
+using Ecommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Services // services are used to communicate between the Web and the Database
+{
+    public class ProductsService
+    {
+        public void SaveProduct(Product product)
+        {
+            using (var context = new CBContext()) // create an object of 'CBContext' 
+            {
+                context.Products.Add(product); // add 'category' argument to the database 'Categories', will be stored in memory not in database yet
+                context.SaveChanges(); // will save it into the database
+            }
+
+        }
+
+        public List<Product> GetProducts()
+        {
+            using (var context = new CBContext())  
+            {
+                return context.Products.ToList(); // return the categories from the database as a list
+
+            }
+
+        }
+
+        public Product GetProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Products.Find(ID); // return the category from the categories database based on the ID
+
+            }
+
+        }
+
+        public void UpdatepProduct(Product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified; // updates the categories database
+                context.SaveChanges(); 
+
+            }
+
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                var productToRemove = context.Products.Find(ID); // find the category to remove based on the ID parameter
+
+                context.Products.Remove(productToRemove); // remove that category
+                context.SaveChanges();
+
+            }
+
+        }
+    }
+}
---
+++ Ecommerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Ecommerce.Entities;
+using Ecommerce.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductsTable(string search) // the value for the 'search' parameter will be recieved through ajax
+        {
+            var products = productsService.GetProducts();
+
+            if (!string.IsNullOrEmpty(search)) // if 'search' parameter is not null or empty
+            {
+                products = products.Where(x => x.Name.ToLower().Contains(search.ToLower())).ToList(); // get the product from database where its name is equal to the 'search' parameter
+
+            }
+
+            // used 'PartialView' becuase we are trying to render this actions View in the Index View, so when we use 'PartialView' it will only render the elements in the Index View from this actions View
+            return PartialView(products); 
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        // this action method is called when we click on the save button
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+
+            productsService.SaveProduct(product); // pass the 'category' argument values to the method 'SaveCategory'
+
+            return RedirectToAction("ProductsTable");
+        }
+    }
+}
