8c54829c702b1b28885c5927cbdb10d69e9d1d15
Nazir <n_ahmad20@live.co.uk>
2019-07-21 18:31:44 +0100

    Add custom Urls in Routeconfig for Category, Product & Shared

--- Ecommerce.Web/App_Start/RouteConfig.cs
+++ Ecommerce.Web/App_Start/RouteConfig.cs
@@ -13,11 +13,86 @@ namespace Ecommerce.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // default route
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
+            //----------------------------------------CATEGORY---------------------------------
+
+            // Categories Table 
+            routes.MapRoute(
+                name: "AllCategories",
+                url: "categories/all",
+                defaults: new { controller = "Category", action = "CategoriesTable" }
+            );
+
+            // Create
+            routes.MapRoute(
+                name: "CreateCategory",
+                url: "categories/create",
+                defaults: new { controller = "Category", action = "Create" }
+            );
+
+            // Edit
+            routes.MapRoute(
+                name: "EditCategory",
+                url: "categories/edit",
+                defaults: new { controller = "Category", action = "Edit", id = UrlParameter.Optional }
+            );
+
+            // Delete
+            routes.MapRoute(
+                name: "DeleteCategory",
+                url: "categories/delete",
+                defaults: new { controller = "Category", action = "Delete" }
+            );
+
+            //----------------------------------------PRODUCT---------------------------------
+
+            // Products Table 
+            routes.MapRoute(
+                name: "AllProducts",
+                url: "products/all",
+                defaults: new { controller = "Product", action = "ProductsTable" }
+            );
+
+            // Create
+            routes.MapRoute(
+                name: "CreateProduct",
+                url: "products/create",
+                defaults: new { controller = "Product", action = "Create" }
+            );
+
+            // Edit
+            routes.MapRoute(
+                 name: "EditProduct",
+                 url: "products/edit",
+                  defaults: new { controller = "Product", action = "Edit", id = UrlParameter.Optional }
+              );
+
+            // Delete
+            routes.MapRoute(
+                 name: "DeleteProduct",
+                 url: "products/delete",
+                 defaults: new { controller = "Product", action = "Delete" }
+             );
+
+
+            //----------------------------------------SHARED---------------------------------
+
+            // UploadImage
+            routes.MapRoute(
+                 name: "UploadImage",
+                 url: "shared/uploadimage",
+                 defaults: new { controller = "Shared", action = "UploadImage" }
+             );
+
+
+
         }
     }
 }
