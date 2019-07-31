e53bbdf206bb4d25f866045ff354009e7692d628
Nazir <n_ahmad20@live.co.uk>
2019-07-31 20:26:38 +0100

    Modify Login & Register

--- Ecommerce.Web/Models/IdentityModels.cs
+++ Ecommerce.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Ecommerce.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EcommerceConnection", throwIfV1Schema: false) // change database connection string to ours so users table can be saved in opur database
         {
         }
 
